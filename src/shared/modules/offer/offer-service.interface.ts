a79a40ae802b1ff0a34064caeb9cb5af3015ae57
lev tarasov <56757711+levil664@users.noreply.github.com>
2024-04-14 21:57:59 +0500

    some fixes module5-task2

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,16 +1,8 @@
 import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { DocumentType } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
-import { UpdateOfferDto } from "./dto/update-offer.dto";
 
 export interface OfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
-  find(): Promise<DocumentType<OfferEntity>[]>;
-  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
-  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
-  findNew(count: number): Promise<DocumentType<OfferEntity>[]>;
-  findPopular(count: number): Promise<DocumentType<OfferEntity>[]>;
-  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
-  exists(documentId: string): Promise<boolean>;
 }
